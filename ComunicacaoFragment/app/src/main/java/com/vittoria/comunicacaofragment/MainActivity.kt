ff312b5c0fbd976dbdf31093ce83ec2621a92c03
Vittoria Cassoni <vittoriacassoni@outlook.com>
2020-10-19 21:54:58 -0300

    Correção da Comunicação entre fragmentos -  teclado não fechava após clicar fora doedit text

--- ComunicacaoFragment/app/src/main/java/com/vittoria/comunicacaofragment/MainActivity.kt
+++ ComunicacaoFragment/app/src/main/java/com/vittoria/comunicacaofragment/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.vittoria.comunicacaofragment
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.fragment_result.*
 import java.util.*
+
 
 class MainActivity : AppCompatActivity(), IResult {
     var inputFragment = FragmentInputs()
@@ -46,5 +53,22 @@ class MainActivity : AppCompatActivity(), IResult {
                 resultFragment.labelResult.text = "$name, você tem $currentAge anos."
             }
         }
+    }
+
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            val v: View? = currentFocus
+            if (v is EditText) {
+                val outRect = Rect()
+                v.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                    v.clearFocus()
+                    val imm: InputMethodManager =
+                        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event)
     }
 }
